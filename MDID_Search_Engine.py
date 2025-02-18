1b1aaf257b309bca7ba982cc9128447df975246e
Didgitalpunk <71613063+Didgitalpunk@users.noreply.github.com>
2025-02-18 16:03:37 +0100

    updated MDID decoder
    added support for BlockWrite, BlockErase, USER memory info, Permalock info, Lock bit info extraction from the XTID.
    The decoding might not be done properly for those new additions, might need reversing the LSB and MSB... needs testing and comparing with either real tag data, or the future GS1 TID decoder.

--- MDID_Search_Engine.py
+++ MDID_Search_Engine.py
@@ -60,15 +60,14 @@ def extract_values(xtid):
     optional_command_present = bin_xtid[int("20", 16)+3] == '1'
     serialization_bits = bin_xtid[int("20", 16):int("20", 16)+3]
 
-
-
+    
     xtid_info += (f"Extended Header Present: {extended_header_present}\n"
                  f"RFU: {rfu_XTID_HEADER}\n"
                  f"User Memory Segment Present: {user_memory_present}\n"
                  f"BlockWrite and BlockErase Segment Present: {block_write_present}\n"
                  f"Optional Command Support Segment Present: {optional_command_present}\n")
-                 
-                 
+
+
     # Calculate serialization info -- 30(h) to 30(h)+48+16*(N - 1) (( or 30(h)+serial_number_length ))
     if serialization_bits != '000':
         N = int(serialization_bits, 2)
@@ -85,8 +84,11 @@ def extract_values(xtid):
 
     xtid_info += (f"Serialization Bits: {serialization_bits}\n" #add the outputs to the display variable
                   f"{serial_number_info}\n{serial_number_output}\n")
-                  
-                  
+
+    if len(xtid)>= 48 and bin_xtid[8] == '1' and serialization_bits != '000':
+        xtid_info +=(f"\n STID URI:  urn:epc:stid:x{MDID_hex}.x{TMN_hex}.x{serial_number_hex}\n\n")# print STID-URI
+
+
     # Optional commands 30(h)+serial_number_length to 3F(h)+serial_number_length
     if optional_command_present == 1:
         bin_max_epc_size = bin_xtid[int("3F", 16)+serial_number_length-4:int("3F", 16)+serial_number_length+1]
@@ -102,6 +104,7 @@ def extract_values(xtid):
         rfu_optional_commands = bin_xtid[int("3F", 16)+serial_number_length-15:int("3F", 16)+serial_number_length-12]
         
         xtid_info +=(f"\n" #add the outputs to the display variable
+                     f" OPTIONAL COMMANDS \n"
                      f"max EPC size bits: {bin_max_epc_size}\n"
                      f"max EPC size: {max_epc_size} bits\n"
                      f"recomissioning support: {recom_support}\n"
@@ -113,11 +116,104 @@ def extract_values(xtid):
                      f"Block Write support: {blockWrite_supported}\n"
                      f"Block Perma-lock support: {blockPermaLock_supported}\n"
                      f"RFU: {rfu_optional_commands}\n")
-        
     else:
         xtid_info +=(f"\n"
                      f"no optional commands\n")
 
+
+    # Block Erase Support 40(h)+serial_number_length to 5F(h)+serial_number_length
+    if blockErase_supported == 1:
+        Block_erase_size = bin_xtid[int("5F",16)+serial_number_length-7:int("5F",16)+serial_number_length+1]
+        Block_erase_variable_size = bin_xtid[int("5F",16)+serial_number_length-8]
+        Block_erase_EPC_offset = bin_xtid[int("5F",16)+serial_number_length-16:int("5F",16)+serial_number_length-8]
+        Block_erase_no_EPC_alignement = bin_xtid[int("5F",16)+serial_number_length-17]
+        Block_erase_User_offset = bin_xtid[int("5F",16)+serial_number_length-25:int("5F",16)+serial_number_length-17]
+        Block_erase_no_User_alignement = bin_xtid[int("5F",16)+serial_number_length-26]
+        Block_erase_RFU = bin_xtid[int("5F",16)+serial_number_length-31:int("5F",16)+serial_number_length-26]
+
+        xtid_info +=(f"\n"
+                     f"Size of Block Erase: {Block_erase_size}\n"
+                     f"Variable size Block Erase bit: {Block_erase_variable_size}\n"
+                     f"Block Erase EPC address offset: {Block_erase_EPC_offset}\n"
+                     f"No Block Erase EPC address alignement: {Block_erase_no_EPC_alignement}\n"
+                     f"Block Erase USER address offset: {Block_erase_User_offset}\n"
+                     f"No Block Erase USER address alignement: {Block_erase_no_User_alignement}\n"
+                     f"RFU: {Block_erase_RFU}\n")
+
+    else:
+        xtid_info +=(f"\n"
+                     f"no Block Erase support\n")
+
+
+    # Block Write Support 60(h)+serial_number_length to 7F(h)+serial_number_length
+    if blockWrite_supported == 1:
+        Block_write_size = bin_xtid[int("7F",16)+serial_number_length-7:int("7F",16)+serial_number_length+1]
+        Block_write_variable_size = bin_xtid[int("7F",16)+serial_number_length-8]
+        Block_write_EPC_offset = bin_xtid[int("7F",16)+serial_number_length-16:int("7F",16)+serial_number_length-8]
+        Block_write_no_EPC_alignement = bin_xtid[int("7F",16)+serial_number_length-17]
+        Block_write_User_offset = bin_xtid[int("7F",16)+serial_number_length-25:int("7F",16)+serial_number_length-17]
+        Block_write_no_User_alignement = bin_xtid[int("7F",16)+serial_number_length-26]
+        Block_write_RFU = bin_xtid[int("7F",16)+serial_number_length-31:int("7F",16)+serial_number_length-26]
+
+        xtid_info +=(f"\n"
+                     f"Size of Block Write: {Block_write_size}\n"
+                     f"Variable size Block Write bit: {Block_write_variable_size}\n"
+                     f"Block Write EPC address offset: {Block_write_EPC_offset}\n"
+                     f"No Block Write EPC address alignement: {Block_write_no_EPC_alignement}\n"
+                     f"Block Write USER address offset: {Block_write_User_offset}\n"
+                     f"No Block Write USER address alignement: {Block_write_no_User_alignement}\n"
+                     f"RFU: {Block_write_RFU}\n")
+
+    else:
+        xtid_info +=(f"\n"
+                     f"no Block Write support\n")
+
+
+    # User Memory info 80(h)+serial_number_length to 8F(h)+serial_number_length
+    if user_memory_present == 1:
+        user_memory_size = bin_xtid[int("80",16)+serial_number_length:int("8F",16)+serial_number_length+1]
+        
+        xtid_info +=(f"\n"
+                     f"USER memory size: {user_memory_size}\n")
+
+    else:
+        xtid_info +=(f"\n"
+                     f"no USER memory present\n")
+
+
+    # Permalock info 90(h)+serial_number_length to 9F(h)+serial_number_length
+    if blockPermaLock_supported == 1:
+        blockPermaLock_size = bin_xtid[int("90",16)+serial_number_length:int("9F",16)+serial_number_length+1]
+        
+        xtid_info +=(f"\n"
+                     f"BlockPermaLock block size: {user_memory_size}\n")
+
+    else:
+        xtid_info +=(f"\n"
+                     f"no BlockPermaLock size described\n")
+
+
+    # Lock bit segment A0(h)+serial_number_length to AF(h)+serial_number_length
+    if S == 1:
+        File0_permalock = bin_xtid[int("AF",16)+serial_number_length-0]
+        File0_PWD_Write = bin_xtid[int("AF",16)+serial_number_length-1]
+        TID_memory_permalock = bin_xtid[int("AF",16)+serial_number_length-2]
+        TID_memory_PWD_Write = bin_xtid[int("AF",16)+serial_number_length-3]
+        EPC_memory_permalock = bin_xtid[int("AF",16)+serial_number_length-4]
+        EPC_memory_PWD_Write = bin_xtid[int("AF",16)+serial_number_length-5]
+        Lock_bit_RFU = bin_xtid[int("AF",16)+serial_number_length-15:int("AF",16)+serial_number_length-5]
+
+        xtid_info +=(f"File_0 memory permalock: {File0_permalock}\n"
+                     f"File_0 memory password write: {File0_PWD_Write}\n"
+                     f"TID memory permalock: {TID_memory_permalock}\n"
+                     f"TID memory password write: {TID_memory_PWD_Write}\n"
+                     f"EPC memory permalock: {EPC_memory_permalock}\n"
+                     f"EPC memory password write: {EPC_memory_PWD_Write}\n"
+                     f"RFU: {Lock_bit_RFU}\n")
+
+    else:
+        xtid_info +=(f"\n"
+                     f"no optional lock bit segment\n")
 
     return decoded_info, xtid_info
 
@@ -127,7 +223,7 @@ class SearchWindow(QWidget):
         super().__init__()
 
         self.setWindowTitle("XTID Search Engine")
-        self.setGeometry(100, 100, 600, 500)
+        self.setGeometry(100, 100, 600, 750)
         
         self.data = data
         self.init_ui()
@@ -165,7 +261,10 @@ class SearchWindow(QWidget):
 
     def to_uppercase(self):
         sender = self.sender()
-        sender.setText(sender.text().upper())
+        cursor_pos = sender.cursorPosition()  # Save cursor position
+        sender.setText(sender.text().upper())  # Modify text
+        sender.setCursorPosition(cursor_pos)  # Restore cursor position
+
 
     def on_search(self):
         xtid = self.xtid_input.text().strip().replace(" ", "")  # Remove spaces
